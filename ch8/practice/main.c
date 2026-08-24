dab8086cb6a0e1649c40be06df43e7b7a0eb99f9
SongDJ <sdj030801@gmail.com>
2026-08-24 16:10:23 +0900

    C언어 문법 학습

---
+++ ch8/practice/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare_int(const void *a, const void *b)
+{
+    if((*(int*)a) < (*(int*)b))
+        return -1;
+    else if((*(int*)a) > (*(int*)b))
+        return 1;
+    else 
+        return 0;
+
+}
+
+
+
+
+
+int main(void)
+{
+
+
+int arr[8] = {2,8,7,1,3,5,6,4};
+qsort(arr, 8, sizeof(int), compare_int);
+
+for(int i =0; i < 8; i++)
+{
+    printf("%d", arr[i]);
+}
+
+}
+
+
